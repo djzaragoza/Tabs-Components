fa34f797d72bf054bc0a5470251f1afa94fa03aa
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-02 19:07:43 -0700

    working on tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -14,7 +14,7 @@ class TabLink {
     // this.tabItem;
     this.tabItem = new tabItem(this.itemElement);
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', evt )
+    this.element.addEventListener('click', evt => this.select(evt) )
   };
 
   select() {
