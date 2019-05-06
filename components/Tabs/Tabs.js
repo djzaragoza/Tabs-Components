5d63770c3403d200db796fb72cd520a676391ef8
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-06 16:47:16 -0700

    modified items.forEach under class TabItem

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -47,10 +47,10 @@ class TabItem {
     // const items;
     const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    items.forEach(item => item.classList.remove('tabs-link-selected'))
+    items.forEach(item => item.classList.remove(`tabs-item-selected`))
     // Add a class named "tabs-item-selected" to this element
     //this.element;
-    this.element.classList.add('tabs-item-selected');
+    this.element.classList.add(`tabs-item-selected`);
   }
 }
 
