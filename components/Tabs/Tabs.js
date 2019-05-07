177f144074d40bb72006833d99ddb69db2a93939
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-06 17:00:17 -0700

    made a few more corrections to class TabItem

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -50,7 +50,7 @@ class TabItem {
     items.forEach(item => item.classList.remove(`tabs-item-selected`))
     // Add a class named "tabs-item-selected" to this element
     //this.element;
-    this.element.classList.add(`tabs-item-selected`);
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
